9e87be3a0f94eddb21871dc33fa8dfa6ced2120c
lup <claudiulupu52@gmail.com>
2023-02-25 21:42:38 +0200

    initial commit

---
+++ device-configuration/src/test/java/com/smartmug/device/configuration/deviceconfiguration/DeviceConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.smartmug.device.configuration.deviceconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
